bc3852fa24e432bf47669f6e119ecab2f0002dad
jagannath18 <jagannath@mobifyi.com>
2021-04-29 12:01:12 +0530

    -- updated token

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,3 @@
 export const APP_ID = "240a33cd836c4e21a9cf1ef7da526f56";
 export const APP_SECRET = "eaf129c80aee40dea448534a5a0b56f2";
-export const TOKEN = "006240a33cd836c4e21a9cf1ef7da526f56IACPqg/SXcROp2ygCZcmmem0wGBIJugQFtmsRz3ktfZJrqqWn64AAAAAEAAF/wL5WouCYAEAAQBai4Jg"
+export const TOKEN = "006240a33cd836c4e21a9cf1ef7da526f56IADDosy8RoFs6bbPLEEBa9ieoADdAw5aQ/MNsF5RhNibCKqWn64AAAAAEAALtir+GqSLYAEAAQAapItg"
